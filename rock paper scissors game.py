b31f032e4f2a0a9f3ba917b83ac34978b0f77bd0
prem12kumar <154820026+prem12kumar@users.noreply.github.com>
2023-12-25 20:28:20 +0530

    Add files via upload

---
+++ rock paper scissors game.py
@@ -0,0 +1,56 @@
+import random
+
+def get_user_choice():
+    while True:
+        user_choice = input("Choose rock, paper, or scissors: ").lower()
+        if user_choice in ["rock", "paper", "scissors"]:
+            return user_choice
+        else:
+            print("Invalid choice. Please choose rock, paper, or scissors.")
+
+def get_computer_choice():
+    return random.choice(["rock", "paper", "scissors"])
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors") or
+        (user_choice == "scissors" and computer_choice == "paper") or
+        (user_choice == "paper" and computer_choice == "rock")
+    ):
+        return "MYrockwin!"
+    else:
+        return "Computer wins!"
+
+def main():
+    user_score = 0
+    computer_score = 0
+
+    print("Welcome to Rock, Paper, Scissors!")
+
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        print(f"MY chose {user_choice}.")
+        print(f"Computer chose {computer_choice}.")
+
+        result = determine_winner(user_choice, computer_choice)
+        print(result)
+
+        if result == "You win!":
+            user_score += 1
+        elif result == "Computer wins!":
+            computer_score += 1
+
+        print(f"MY score: {user_score}")
+        print(f"Computer's score: {computer_score}")
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            break
+
+    print("Thanks for playing!")
+
+if __name__ == "__main__":
+    main()
